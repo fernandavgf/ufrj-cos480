ea1e44a7cda4855e1d38a44b588f288c43a14ec1
gabrielmannarino <gabrielmannarino@gmail.com>
2022-01-16 23:47:02 -0300

    Adiciona os bancos gerados pelas estruturas

--- dataFixedHeap.h
+++ dataFixedHeap.h
@@ -1,6 +1,5 @@
-File structure: Ordered
-Creation: 2022-01-16 22:42:10
-Last modification: 2022-01-16 22:42:10
+File structure: Heap
+Creation: 2022-01-16 23:32:08
+Last modification: 2022-01-16 23:32:52
 Schema: COD-INTEGER(4)|SG_ENTIDADE_ENSINO-VARCHAR(20)|NM_ENTIDADE_ENSINO-VARCHAR(86)|CD_CURSO_PPG-VARCHAR(13)|NM_CURSO-VARCHAR(110)|
 Number of records: 6494
-
--- dataOrdered.h
+++ dataOrdered.h
@@ -1,6 +1,5 @@
 File structure: Ordered
-Creation: 2022-01-16 22:42:10
-Last modification: 2022-01-16 22:42:10
+Creation: 2022-01-16 23:33:38
+Last modification: 2022-01-16 23:33:38
 Schema: COD-INTEGER(4)|SG_ENTIDADE_ENSINO-VARCHAR(20)|NM_ENTIDADE_ENSINO-VARCHAR(86)|CD_CURSO_PPG-VARCHAR(13)|NM_CURSO-VARCHAR(110)|
 Number of records: 6494
-
